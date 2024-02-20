7e81870359f18ea3ab311e18a7d84eb713e1f012
ZoltaanDzvonyaar <zoltandzvonyar@gmail.com>
2024-02-20 18:45:21 +0100

    last commit

--- elso.py
+++ elso.py
@@ -1,5 +1,5 @@
 uzenet="helloworld"
-kulcs="abcdefgjikl"
+kulcs="abcdefgijkl"
 
 def rejtjelez(uzenet,kulcs):
     rejtjelezett_u=""
@@ -27,3 +27,35 @@ def rejtjelez(uzenet,kulcs):
 
 atirtuzenet=rejtjelez(uzenet,kulcs)
 print(atirtuzenet)
+
+
+def visszafejtes(atirtuzenet,kulcs):
+    visszafejtettuzenet=""
+    kulcs_karakter_indexe=0
+    for karakter in atirtuzenet:
+        if karakter == " ":
+            karakterkodja=26
+
+        else:
+            karakterkodja = ord(karakter) - ord("a")
+
+        if kulcs_karakter_indexe>=len(kulcs):
+            kulcs_karakter_indexe = 0
+
+        kulcs_karakterjeinek_kodja = ord(kulcs[kulcs_karakter_indexe]) - ord ("a")
+        
+        kulcs_karakter_indexe+=1
+        
+
+        uzenet_kulcs_kod_kulonbseg=(karakterkodja - kulcs_karakterjeinek_kodja)%27
+        
+        if uzenet_kulcs_kod_kulonbseg < 0:
+            uzenet_kulcs_kod_kulonbseg += 27
+        if uzenet_kulcs_kod_kulonbseg == 26:
+            visszafejtettuzenet += " "
+        else:
+            visszafejtettuzenet += chr(uzenet_kulcs_kod_kulonbseg + ord("a") )
+    return visszafejtettuzenet
+
+visszairtuzenet=visszafejtes(atirtuzenet,kulcs)
+print(visszairtuzenet)
