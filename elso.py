ef3a1f4421b06a55c83e0ec05ff137314343c62a
ZoltaanDzvonyaar <zoltandzvonyar@gmail.com>
2024-02-20 17:36:55 +0100

    second commit

--- elso.py
+++ elso.py
@@ -2,3 +2,20 @@ uzenet="helloworld"
 kulcs="abcdefgjikl"
 
 def rejtjelez(uzenet,kulcs):
+    rejtjelezett_u=""
+    kulcs_karakter_indexe=0
+    for karakter in uzenet:
+        if karakter == " ":
+            karakterkodja=26
+
+        else:
+            karakterkodja=ord(karakter) - ord("a") #a erteke 97, 0-25 tartomanyban marad.
+
+        if kulcs_karakter_indexe>=len(kulcs):
+            kulcs_karakter_indexe=0
+
+        kulcs_karakterjeinek_kodja=ord(kulcs[kulcs_karakter_indexe]) - ord ("a")
+
+        kulcs_karakter_indexe+=1
+
+        
